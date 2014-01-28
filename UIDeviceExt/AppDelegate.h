f21bf3ac4b3b757c84381cf55acf05cde96db4b2
Jens Schwarzer <schwarzer@schwarzer.dk>
2014-01-28 22:22:32 +0100

    Initial Commit

---
+++ UIDeviceExt/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIDeviceExt
+//
+//  Created by Jens Schwarzer on 28/01/14.
+//  Copyright (c) 2014 marchv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIDeviceExt/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UIDeviceExt
+//
+//  Created by Jens Schwarzer on 28/01/14.
+//  Copyright (c) 2014 marchv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
